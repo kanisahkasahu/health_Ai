f629911a036c53b8cbaab1076b40f4b38d8d65be
kanishkasahu <kanishkasahu005@gmail.com>
2019-11-16 16:19:39 +0530

    secondcommit

--- flask_blog/flaskblog.py
+++ flask_blog/flaskblog.py
@@ -1,10 +1,47 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "hellow world!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+posts = [
 
+    {
+        'author': 'kanishka sahu',
+        'title': 'post titile',
+        'content': 'post content',
+        'date': '21 Sep 2019'
+    },
+     {
+        'author': 'kanishka sahu',
+        'title': 'post titile',
+        'content': 'post content',
+        'date': '21 Sep 2019'
+    },
+     {
+        'author': 'kanishka sahu',
+        'title': 'post titile',
+        'content': 'post content',
+        'date': ' 21 Sep 2019'
+    },
+     {
+        'author': 'kanishka sahu',
+        'title': 'post titile',
+        'content': 'post content',
+        'date': '21 Sep 2019'
+    }
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('layout.html',posts=posts)
+
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+ 
+
+if __name__=='__main__':
+    app.run(debug=True) 

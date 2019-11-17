9330fee737902cb348c0a054d381d22b32d0e18e
ShivamGoyal26 <officialshivamgoyal@gmail.com>
2019-11-17 09:55:30 +0530

    seventh commit

--- flask_blog/flaskblog.py
+++ flask_blog/flaskblog.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask, render_template, url_for,request,flash
+import tensorflow as tf
+from flask import Flask, render_template, url_for,request,flash,redirect
+from forms import RegistrationForm, LoginForm
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -13,6 +15,7 @@ from flask_wtf import FlaskForm
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 from datetime import date
 from _thread import *  
+from keras.models import model_from_json
 app = Flask(__name__)
 
 
@@ -54,6 +57,25 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
 @app.route('/p_form',methods=['GET','POST'])
 def p_form():
@@ -118,13 +140,12 @@ class ReusableForm(Form):
             # model = pickle.load(pickle_in)
             # prediction=model.predict(final)
             #print(name,day,month)
+            json_string='{"class_name": "Sequential", "config": {"name": "sequential_1", "layers": [{"class_name": "Dense", "config": {"name": "dense_1", "trainable": true, "batch_input_shape": [null, 11], "dtype": "float32", "units": 25, "activation": "softsign", "use_bias": true, "kernel_initializer": {"class_name": "VarianceScaling", "config": {"scale": 1.0, "mode": "fan_avg", "distribution": "uniform", "seed": null}}, "bias_initializer": {"class_name": "Zeros", "config": {}}, "kernel_regularizer": null, "bias_regularizer": null, "activity_regularizer": null, "kernel_constraint": {"class_name": "MaxNorm", "config": {"max_value": 2, "axis": 0}}, "bias_constraint": null}}, {"class_name": "Dense", "config": {"name": "dense_2", "trainable": true, "dtype": "float32", "units": 5, "activation": "softsign", "use_bias": true, "kernel_initializer": {"class_name": "VarianceScaling", "config": {"scale": 1.0, "mode": "fan_avg", "distribution": "uniform", "seed": null}}, "bias_initializer": {"class_name": "Zeros", "config": {}}, "kernel_regularizer": null, "bias_regularizer": null, "activity_regularizer": null, "kernel_constraint": null, "bias_constraint": null}}, {"class_name": "Dense", "config": {"name": "dense_3", "trainable": true, "dtype": "float32", "units": 3, "activation": "softsign", "use_bias": true, "kernel_initializer": {"class_name": "VarianceScaling", "config": {"scale": 1.0, "mode": "fan_avg", "distribution": "uniform", "seed": null}}, "bias_initializer": {"class_name": "Zeros", "config": {}}, "kernel_regularizer": null, "bias_regularizer": null, "activity_regularizer": null, "kernel_constraint": null, "bias_constraint": null}}, {"class_name": "Dense", "config": {"name": "dense_4", "trainable": true, "dtype": "float32", "units": 1, "activation": "sigmoid", "use_bias": true, "kernel_initializer": {"class_name": "VarianceScaling", "config": {"scale": 1.0, "mode": "fan_avg", "distribution": "uniform", "seed": null}}, "bias_initializer": {"class_name": "Zeros", "config": {}}, "kernel_regularizer": null, "bias_regularizer": null, "activity_regularizer": null, "kernel_constraint": null, "bias_constraint": null}}]}, "keras_version": "2.3.1", "backend": "tensorflow"}'
+            modeltopredict = model_from_json(json_string)
+            modeltopredict.load_weights("data/weights.hdf5", by_name=False)
+            prediction=modeltopredict.predict(final)
+            print(prediction)
             
-           
-            pickle_in = open("data/model.pickle","rb")
-            model = pickle.load(pickle_in)
-
-    
-            prediction=model.predict(final)
         if form.validate():
             # Save the comment here.
             flash('Hello ' + name)
